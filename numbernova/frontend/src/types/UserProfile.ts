d4e9a9100a1d9fdc917f5ac0d54ca12c574a0fb7
Suldana Abdulkadir Afrah <sabdulkadirafrah@ucsd.edu>
2025-11-08 22:55:39 -0800

    resolving merge conflicts

--- numbernova/frontend/src/types/UserProfile.ts
+++ numbernova/frontend/src/types/UserProfile.ts
@@ -1,11 +1,16 @@
+import { Database } from './database';
+
 export interface UserProfile {
-    id: string;
-    username?: string;
-    profile_name?: string;
-    score: number;
-    rank?: number;
-    level?: number;
-    ship_color?: string;
-    tokens?: number;
-    profile_picture_url?: string;
+    id: Database['public']['Tables']['user_profiles']['Row']['id'];
+    score: Database['public']['Tables']['user_profiles']['Row']['score'];
+    level: Database['public']['Tables']['user_profiles']['Row']['level'];
+    ship_color: Database['public']['Tables']['user_profiles']['Row']['ship_color'];
+    rank: Database['public']['Tables']['user_profiles']['Row']['rank'];
+    created_at: Database['public']['Tables']['user_profiles']['Row']['created_at'];
+    updated_at: Database['public']['Tables']['user_profiles']['Row']['updated_at'];
+    profile_name: Database['public']['Tables']['user_profiles']['Row']['profile_name'];
+    profile_picture_url: Database['public']['Tables']['user_profiles']['Row']['profile_picture_url'];
+    games_played: Database['public']['Tables']['user_profiles']['Row']['games_played'];
+    games_won: Database['public']['Tables']['user_profiles']['Row']['games_won'];
+    tokens: Database['public']['Tables']['user_profiles']['Row']['tokens'];
 }
